85d98a4e6fe563c1dfd6cfd51a9e17282f0a6881
Mukhamed Karanashev <menchauser@gmail.com>
2021-12-01 16:08:12 +0300

    Reorganize file

--- day1.lisp
+++ day1.lisp
@@ -1,27 +1,26 @@
-;; Part 1
-(defun sum-increases (xs)
+(defun sum (xs)
+  (reduce #'+ xs))
+
+(defun count-increases (xs)
+	"Count the number of increases between previous and current list element"
   (let ((diffs (mapcar #'- (cdr xs) xs)))
     (count-if #'plusp diffs)))
 
-(defun part1 (path)
-  (let* ((lines (uiop:read-file-lines path))
-         (numbers (mapcar #'parse-integer lines)))
-    (sum-increases numbers)))
-
-
-;; Part 2
 (defun group-by (xs n)
   "Groups list in sublists with n-elements each"
   (loop for i from 0 to (- (length xs) n) by 1
         collect (subseq xs i (+ i n))))
 
-(defun sum (xs)
-  (reduce #'+ xs))
+
+(defun part1 (path)
+  (let* ((lines (uiop:read-file-lines path))
+         (numbers (mapcar #'parse-integer lines)))
+    (count-increases numbers)))
 
 (defun part2 (path)
   (let* ((lines (uiop:read-file-lines path))
          (numbers (mapcar #'parse-integer lines))
          (groups (group-by numbers 3))
          (measurements (mapcar #'sum groups)))
-    (sum-increases measurements)))
+    (count-increases measurements)))
 
