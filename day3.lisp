8c5cdc2d18bb9e3a65abb49987b207c2968220a7
Mukhamed Karanashev <menchauser@gmail.com>
2021-12-03 11:43:37 +0300

    Day 3 part 2 dirty solution

--- day3.lisp
+++ day3.lisp
@@ -37,6 +37,78 @@
 				 (gamma (to-number gamma-digits))
 				 (epsilon (to-number epsilon-digits)))
 		(* gamma epsilon)))
-				 
+
+(defun count-elements (xs)
+	"Gets a list and returns alist of (element . count) where count is number of 
+that element in string."
+	(let ((counts nil))
+		(dolist (x xs)
+			(if (assoc x counts)
+					(incf (cdr (assoc x counts)))
+					(setf counts (cons (cons x 1) counts))))
+		counts))
+
+(defun max-element (xs &key (key< #'<) (value< #'<))
+	"Gets an alist and finds maximum element according to element values. The 
+values should be non-negative. If values are equal: element with bigger key is 
+selected (so char #\1 > #\0)."
+	(if (eql 1 (length xs))
+			(car xs)
+			(reduce
+			 (lambda (x y)
+				 (cond
+					 ((funcall value< (cdr x) (cdr y)) y)
+					 ((eql (cdr x) (cdr y)) (if (funcall key< (car x) (car y)) y x))
+					 (t x)))
+			 (cdr xs)
+			 :initial-value (car xs))))
+
+(defun most-common (strings position)
+	"Returns character which is most common in the given position of passed 
+strings"
+	(let* ((chars (mapcar (lambda (s) (char s position)) strings))
+				 (counts (count-elements chars)))
+		(car (max-element counts :key< #'char<))))
+
+(defun least-common (strings position)
+	"Returns character which is least common in the given position of passed 
+STRINGS."
+	(let* ((chars (mapcar (lambda (s) (char s position)) strings))
+				 (counts (count-elements chars)))
+		(car (max-element counts :key< #'char> :value< #'>))))
+
+(defun filter-strings (strings ch position)
+	"Select only strings with the character CH in given POSITION."
+	(remove-if-not
+	 (lambda (str) (eql ch (char str position)))
+	 strings))
+
+(defun part2-loop (lines char-function position)
+	;; if only one line remains - return it
+	;; otherwise: filter lines by next position and make step
+	(format nil "Filtering lines ~a position ~a~%" lines position)
+	(cond
+		((> position (length (car lines)))
+		 (error "position is outside of string length"))
+		((<= (length lines) 1) (car lines))
+		(t
+		 (let ((ch (funcall char-function lines position)))
+			 (format nil "Selected character: ~a~%" ch)
+			 (part2-loop
+				(filter-strings lines ch position)
+				char-function
+				(1+ position))))))
+
+(defun part2-max-loop (lines)
+	(part2-loop lines #'most-common 0))
 		
-		
+(defun part2-min-loop (lines)
+	(part2-loop lines #'least-common 0))
+
+;; Now we start working the list of symbols until either the string ends or only
+;; one string remains
+(defun part2 (path)
+	(let* ((lines (uiop:read-file-lines path))
+				 (oxygen-value (parse-integer (part2-max-loop lines) :radix 2))
+				 (co2-value (parse-integer (part2-min-loop lines) :radix 2)))
+		(* oxygen-value co2-value)))
