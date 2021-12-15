d15470ed795968a3cbd672bd2bae74f5f776860b
Mukhamed Karanashev <menchauser@gmail.com>
2021-12-15 23:12:41 +0300

    Finished day 15 part 1

--- day15.lisp
+++ day15.lisp
@@ -7,33 +7,64 @@
                                   (coerce line 'list)))
                         lines))))
 
+(defun print-array (arr)
+  (loop for r from 0 below (array-dimension arr 0) do
+    (loop for c from 0 below (array-dimension arr 1) do
+      (format t "~4d" (aref arr r c)))
+    (format t "~%")))
+
+(defun neighbours (cave row col)
+  (let ((rows (array-dimension cave 0))
+        (cols (array-dimension cave 1)))
+    ;; final point has no neighbours
+    (if (equal (cons row col) (cons (1- rows) (1- cols)))
+        nil
+        (remove-if
+         (lambda (x) (or
+                      (< (car x) 0)         ; over the top row
+                      (>= (car x) rows)     ; over the bottom row
+                      (< (cdr x) 0)         ; over the left col
+                      (>= (cdr x) cols)     ; over the right col
+                      ;; there is no point to go left on edge rows
+                      (and (< (cdr x) col)
+                           (or (= (car x) 0) (= (car x) (1- cols))))
+                      ;; there is no point to go up on the edge cols
+                      (and (< (car x) row)
+                           (or (= (cdr x) 0) (= (cdr x) (1- cols))))))
+         (list (cons (1- row) col)
+               (cons row (1+ col))
+               (cons (1+ row) col)
+               (cons row (1- col)))))))
+    
+(defun shortest-path-dijkstra (cave)
+  "Find shortest path from top-left to bottom-right corner. Current position is
+encoded as POS cons cell."
+  (let ((max-row (1- (array-dimension cave 0)))
+        (max-col (1- (array-dimension cave 1)))
+        (queue nil)
+        (distances (make-array (array-dimensions cave))))
+    (loop for row from 0 to max-row do
+      (loop for col from 0 to max-col
+            do (setf (aref distances row col) most-positive-fixnum)
+               (push (cons row col) queue)))
+    (setf (aref distances 0 0) 0)
+    (setf queue (sort queue (lambda (x y)
+                          (< (aref distances (car x) (cdr x))
+                             (aref distances (car y) (cdr y))))))
+    (loop for n = (pop queue)
+          while n
+            do (loop for neighbour in (neighbours cave (car n) (cdr n))
+                     for new-length = (+ (aref distances (car n) (cdr n))
+                                         (aref cave (car neighbour) (cdr neighbour)))
+                     for old-length = (aref distances (car neighbour) (cdr neighbour))
+                     when (< new-length old-length)
+                       do (setf (aref distances (car neighbour) (cdr neighbour))
+                                new-length))
+               (setf queue (sort queue (lambda (x y)
+                                         (< (aref distances (car x) (cdr x))
+                                            (aref distances (car y) (cdr y)))))))
+    (aref distances max-row max-col)))
+
 (defun day15 (path)
   (let ((cave (read-input path)))
-    ;; we start at bottom-right position and go top-left until 1-1
-    (loop with max-row = (1- (array-dimension test15 0))
-          with max-col = (1- (array-dimension test15 1))
-          with row = 0
-          with col = 0
-          with risk = (aref cave row col)
-          do (progn
-               (cond
-                 ((and (= row max-row) (= col max-col)) (return risk))
-                 ;; if we hit top or left line: just move along it
-                 ((= row max-row)
-                  (incf col)
-                  (format t "(hor) next step: ~a, ~a; delta: ~a~%"
-                          row col (aref cave row col))
-                  (incf risk (aref cave row col)))
-                 ((= col max-col)
-                  (incf row)
-                  (format t "(ver) next step: ~a, ~a; delta: ~a~%"
-                          row col (aref cave row col))
-                  (incf risk (aref cave row col)))
-                 ;; otherwise: select cell with minimal risk
-                 (t (if (< (aref cave row (1+ col)) (aref cave (1+ row) col))
-                        (incf col)
-                        (incf row))
-                    (format t "next step: ~a, ~a; delta: ~a~%"
-                            row col (aref cave row col))
-                    (incf risk (aref cave row col)))))
-          finally (return risk))))
+    (shortest-path-dijkstra cave)))
