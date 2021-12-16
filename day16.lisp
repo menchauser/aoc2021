6a26855881c7c247e3df80de6cf6d411bed73108
Mukhamed Karanashev <menchauser@gmail.com>
2021-12-16 11:56:44 +0300

    Day 16 part 1

--- day16.lisp
+++ day16.lisp
@@ -1,14 +1,22 @@
 (defun to-binary (hex-str)
-  (format nil "~b" (parse-integer hex-str :radix 16)))
+  (loop for c across hex-str
+        collect (format nil "~4,'0b" (parse-integer (string c) :radix 16))
+          into parts
+        finally (return (reduce (lambda (a b) (concatenate 'string a b)) parts))))
 
 (defun bin (bin-str)
   (parse-integer bin-str :radix 2))
 
+(defstruct literal
+  version value)
 
-(defun read-literal (bits &key start)
+(defstruct op
+  version type length-type length subpackets)
+
+(defun read-literal-value (bits &key start (finalize nil))
   "START position is set to the beginning of first group of literal bits. 
-Returns literal value and position after the end."
-  ;; lets read next 5 bits
+FINALIZE demands to consume zeroes after the final group. Returns literal value 
+and position after the end."
   ;;(format t "~a~%" bits)
   ;;(format t "~v@{~a~:*~}" start #\Space)
   (loop with tag = #\A
@@ -16,39 +24,41 @@ Returns literal value and position after the end."
         with pos = start
         for group-bit = (bin (subseq bits pos (1+ pos)))
         for group = (subseq bits (1+ pos) (+ 5 pos))
-        do (format t "~v@{~a~:*~}" 5 tag)
+        do ;; (format t "~v@{~a~:*~}" 5 tag)
            (setf value (+ (* value 16) (bin group))
                  pos (+ pos 5)
                  tag (code-char (1+ (char-code tag))))
         if (= 0 group-bit)
+          ;; (- pos start) should be divisible by 4
           ;; count final zeroes to add to pos
-          return (loop for p from pos below (length bits)
-                       while (eql (elt bits p) #\0)
-                       do (format t "-")
-                       finally (return (values value p)))))
+          return (if finalize 
+                     (let ((pos-mod (mod pos 4)))
+                       (case pos-mod
+                         (0 (values value pos))
+                         (otherwise (loop repeat (- 4 pos-mod)
+                                          do (format t "-")
+                                             (incf pos)
+                                          finally (return (values value pos))))))
+                     (values value pos))))
   
 (defun read-packet (bits &key (start 0))
-  (format t "~&~a~%" (subseq bits start))
-  (format t "VVVTTT")
+  ;; (format t "~&~a~%" (subseq bits start))
+  ;; (format t "VVVTTT")
   (let ((version (bin (subseq bits start (+ start 3))))
         (type (bin (subseq bits (+ start 3) (+ start 6)))))
-    ;; we have version and type
-    ;; now we have to determine where packet ends
-    ;; for literal value we read groups of 5 bits until we reach one with 0
-    ;; for operator
     (case type
       (4
        (multiple-value-bind (value pos)
-           (read-literal bits :start (+ start 6))
-         (values (list 'literal version type value)
+           (read-literal-value bits :start (+ start 6))
+         (values (make-literal :version version :value value)
                  pos)))
       (otherwise
        (let ((length-type (bin (subseq bits (+ start 6) (+ start 7)))))
-         (format t "I")
+         ;; (format t "I")
          (case length-type
            ;; next 15 bits - total length
            (0
-            (format t "~v@{~a~:*~}" 15 "L")
+            ;; (format t "~v@{~a~:*~}" 15 "L")
             (let ((total-length (bin (subseq bits (+ start 7) (+ start 7 15)))))
               ;; (format t "~&start position: ~a, end:~a~%"
               ;;         (+ start 7 15) (+ start 7 15 total-length))
@@ -58,16 +68,16 @@ Returns literal value and position after the end."
                                                 (read-packet bits :start pos))
                     do (setf pos next-pos)
                     collect subpacket into subpackets
-                    finally (return (values (list 'op
-                                                  version
-                                                  type
-                                                  length-type
-                                                  total-length
-                                                  subpackets)
+                    finally (return (values (make-op
+                                             :version version
+                                             :type type
+                                             :length-type length-type
+                                             :length total-length
+                                             :subpackets subpackets)
                                             pos)))))
            ;; next 11 bits - number of sub-packets 
            (1
-            (format t "~v@{~a~:*~}" 11 "L")
+            ;; (format t "~v@{~a~:*~}" 11 "L")
             (let ((total-count (bin (subseq bits (+ start 7) (+ start 7 11)))))
               (loop with pos = (+ start 7 11)
                     repeat total-count
@@ -75,10 +85,22 @@ Returns literal value and position after the end."
                                                 (read-packet bits :start pos))
                     do (setf pos next-pos)
                     collect subpacket into subpackets
-                    finally (return (values (list 'op
-                                                  version
-                                                  type
-                                                  length-type
-                                                  total-count
-                                                  subpackets)
+                    finally (return (values (make-op
+                                             :version version
+                                             :type type
+                                             :length-type length-type
+                                             :length total-count
+                                             :subpackets subpackets)
                                             pos)))))))))))
+
+(defun sum-versions (packet)
+  (cond
+    ((literal-p packet) (literal-version packet))
+    (t (+ (op-version packet)
+          (loop for sub in (op-subpackets packet)
+                sum (sum-versions sub))))))
+
+(defun part1 (path)
+  (let* ((line (uiop:read-file-line path))
+         (packet (read-packet (to-binary line))))
+    (sum-versions packet)))
